fa12e99bb75fc911c9a4e5cf289b8b5dadb287de
Robert Masen <r.f.masen@gmail.com>
2024-10-19 12:11:53 -0500

    simpler init?

--- octoprint_octosse/__init__.py
+++ octoprint_octosse/__init__.py
@@ -140,23 +140,6 @@ class OctosseCallback(octoprint.printer.PrinterCallback):
             logger.debug(f"error in on_printer_send_current_data, unregestering: {ex}")
             self.printer.unregister_callback(self)
 
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "Octosse Plugin"
-
-
-# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
-__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
-
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = OctossePlugin()
-
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        # "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
+__plugin_pythoncompat__ = ">=3,<4"
+__plugin_implementation__ = OctossePlugin()
