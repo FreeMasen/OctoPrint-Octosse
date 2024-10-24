9acad5dfb269b2c0034ac0e986b07cf49f9e0700
Robert Masen <r.f.masen@gmail.com>
2024-10-23 21:09:01 -0500

    ...

--- octoprint_octosse/__init__.py
+++ octoprint_octosse/__init__.py
@@ -55,7 +55,7 @@ class OctossePlugin(
         initial_data = self.get_initial_info()
         q = queue.Queue()
         self.queues.append(q)
-        q.put_nowait(initial_data)
+        # q.put_nowait(initial_data)
         res = flask.Response(
             create_generator(q),
             mimetype="text/event-stream",
